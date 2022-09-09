6463458951137fb93d3999ce84118323b714ab19
Jorge Hernandez <jdaviderb@gmail.com>
2022-09-09 00:49:45 -0500

    add create_oracle and update_oracle_rpc

--- programs/dejavu-football/src/lib.rs
+++ programs/dejavu-football/src/lib.rs
@@ -2,29 +2,108 @@ use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+
 #[program]
 pub mod dejavu_football {
     use super::*;
 
-    pub fn create_authorizer(ctx: Context<AuthInstruction>, _auth_id: i64) -> Result<()> {
+    pub fn create_authorizer(
+        ctx: Context<CreateAuthorizerInstruction>,
+        _auth_id: i64,
+    ) -> Result<()> {
         ctx.accounts.authorizer.authority = *ctx.accounts.user.key;
-        
+        Ok(())
+    }
+
+    pub fn update_oracle(
+        ctx: Context<UpdateOracleInstruction>,
+        team_a_value: u8,
+        team_b_value: u8,
+    ) -> Result<()> {
+
+        // set team ids
+        ctx.accounts.oracle.results[0] = team_a_value;
+        ctx.accounts.oracle.results[1] = team_b_value;
+        Ok(())
+    }
+
+    pub fn create_oracle(
+        ctx: Context<CreateOracleInstruction>,
+        _auth_id: i64,
+        team_a_id: u8,
+        team_b_id: u8,
+        closed_at: i64,
+        finished_at: i64,
+    ) -> Result<()> {
+        ctx.accounts.oracle.authorizer = ctx.accounts.authorizer.key();
+
+        // set team ids
+        ctx.accounts.oracle.team_ids[0] = team_a_id;
+        ctx.accounts.oracle.team_ids[1] = team_b_id;
+        // set timestamp medatadata
+        ctx.accounts.oracle.closed_at = closed_at;
+        ctx.accounts.oracle.finished_at = finished_at;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {
+#[account]
+pub struct AuthorizerAccount {
+    authority: Pubkey, // 32
 }
 
 #[account]
-pub struct AuthorizerAccount {
-    authority: Pubkey // 32
+pub struct Oracle {
+    authorizer: Pubkey, // 32
+    team_ids: [u8; 2],  // minimum 2 rows = 4 bytes size of array  + 2 byte = 6
+    results: [u8; 2],   // minimum 2 rows = 4 bytes of size of array + 2 byte = 6
+    closed_at: i64,     // 8
+    finished_at: i64,   // 8
+    is_finished: bool,  // 1
+    is_invalid: bool // 1
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+#[derive(Accounts)]
+#[instruction(oracle_id: i64)]
+pub struct CreateOracleInstruction<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32 + 2 + 2 + 8 + 8 + 1 + 1,
+        seeds = [authorizer.key().as_ref(), format!("id-{}", oracle_id).as_bytes().as_ref()], 
+        bump,
+        constraint = authorizer.authority == *user.key,
+    )]
+    oracle: Account<'info, Oracle>,
+    authorizer: Account<'info, AuthorizerAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateOracleInstruction<'info> {
+    #[account(
+        mut,
+        constraint = oracle.authorizer == *authorizer.to_account_info().key,
+        constraint = authorizer.authority == *user.to_account_info().key,
+        constraint = oracle.is_invalid == false,
+        constraint = oracle.is_finished == false
+    )]
+    oracle: Account<'info, Oracle>,
+    authorizer: Account<'info, AuthorizerAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 #[instruction(auth_id: i64)]
-pub struct AuthInstruction<'info> {
+pub struct CreateAuthorizerInstruction<'info> {
     #[account(
         init,
         payer = user,
@@ -37,5 +116,3 @@ pub struct AuthInstruction<'info> {
     user: Signer<'info>,
     system_program: Program<'info, System>,
 }
-
-
--- tests/dejavu-football.ts
+++ tests/dejavu-football.ts
@@ -30,4 +30,141 @@ describe("dejavu-football", () => {
     });
   });
 
+  describe('#create-oracle', async () => {
+    it("creates an oracle", async () => {
+      // create an auhorizer
+      const authId = new Date().getTime();
+      const closedAt = new Date().getTime();
+      const finishedAt = new Date().getTime() + 1;
+      
+      const [authorizer] = await anchor.web3.PublicKey.findProgramAddress(
+        [provider.wallet.publicKey.toBuffer(), Buffer.from(`id-${authId}`)],
+        program.programId
+      );
+  
+      await program.methods.createAuthorizer(new BN(authId)).accounts({
+        authorizer: authorizer,
+        user: provider.wallet.publicKey
+      }).rpc();
+
+      // create an oracle
+
+      const [oracle] = await anchor.web3.PublicKey.findProgramAddress(
+        [authorizer.toBuffer(), Buffer.from(`id-${authId}`)],
+        program.programId
+      );
+
+      await program.methods.createOracle(
+        new BN(authId), // oracle id
+        1, // team_a id
+        2,  // team_b id
+        new BN(closedAt), // closed_at timestamp, 
+        new BN(finishedAt) // finished_at timestamp
+      ).accounts({
+        authorizer: authorizer,
+        oracle: oracle,
+        user: provider.wallet.publicKey
+      }).rpc();
+
+      const oracleData = await program.account.oracle.fetch(oracle);
+
+      assert.ok(
+        oracleData.authorizer.equals(authorizer)
+      );
+
+      assert.ok(
+        oracleData.closedAt.eq(new BN(closedAt))
+      );
+
+      assert.ok(
+        oracleData.finishedAt.eq(new BN(finishedAt))
+      );
+
+      assert.ok(
+        oracleData.teamIds[0] === 1
+      );
+
+      assert.ok(
+        oracleData.teamIds[1] === 2
+      );
+    });
+  });
+
+  describe('#update-oracle', async () => {
+    it("updates an oracle", async () => {
+      // create an auhorizer
+
+      const authId = new Date().getTime();
+      const closedAt = new Date().getTime();
+      const finishedAt = new Date().getTime() + 1;
+      
+      const [authorizer] = await anchor.web3.PublicKey.findProgramAddress(
+        [provider.wallet.publicKey.toBuffer(), Buffer.from(`id-${authId}`)],
+        program.programId
+      );
+  
+      await program.methods.createAuthorizer(new BN(authId)).accounts({
+        authorizer: authorizer,
+        user: provider.wallet.publicKey
+      }).rpc();
+
+      // create an oracle
+
+      const [oracle] = await anchor.web3.PublicKey.findProgramAddress(
+        [authorizer.toBuffer(), Buffer.from(`id-${authId}`)],
+        program.programId
+      );
+
+      await program.methods.createOracle(
+        new BN(authId), // oracle id
+        20, // team_a id
+        25,  // team_b id
+        new BN(closedAt), // closed_at timestamp, 
+        new BN(finishedAt) // finished_at timestamp
+      ).accounts({
+        authorizer: authorizer,
+        oracle: oracle,
+        user: provider.wallet.publicKey
+      }).rpc();
+
+      // update oracle
+
+      await program.methods.updateOracle(1, 2).accounts({
+        authorizer: authorizer,
+        oracle: oracle,
+        user: provider.wallet.publicKey
+      }).rpc();
+
+      const oracleData = await program.account.oracle.fetch(oracle);
+
+      assert.ok(
+        oracleData.authorizer.equals(authorizer)
+      );
+
+      assert.ok(
+        oracleData.closedAt.eq(new BN(closedAt))
+      );
+
+      assert.ok(
+        oracleData.finishedAt.eq(new BN(finishedAt))
+      );
+
+      assert.ok(
+        oracleData.teamIds[0] === 20
+      );
+
+      assert.ok(
+        oracleData.teamIds[1] === 25
+      );
+
+      assert.ok(
+        oracleData.results[0] === 1
+      );
+
+      assert.ok(
+        oracleData.results[1] === 2
+      );
+    });
+  });
+
 });
