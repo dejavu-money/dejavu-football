7d2699a14f484d970f0959f3271b87ed40d0064c
Jorge Hernandez <jdaviderb@gmail.com>
2022-09-08 22:26:08 -0500

    add create_authorizer rpc

--- programs/dejavu-football/src/lib.rs
+++ programs/dejavu-football/src/lib.rs
@@ -6,10 +6,36 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod dejavu_football {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_authorizer(ctx: Context<AuthInstruction>, _auth_id: i64) -> Result<()> {
+        ctx.accounts.authorizer.authority = *ctx.accounts.user.key;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize {
+}
+
+#[account]
+pub struct AuthorizerAccount {
+    authority: Pubkey // 32
+}
+
+#[derive(Accounts)]
+#[instruction(auth_id: i64)]
+pub struct AuthInstruction<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 32,
+        seeds = [user.key().as_ref(), format!("id-{}", auth_id).as_bytes().as_ref()], 
+        bump
+    )]
+    authorizer: Account<'info, AuthorizerAccount>,
+    #[account(mut)]
+    user: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+
--- tests/dejavu-football.ts
+++ tests/dejavu-football.ts
@@ -1,16 +1,33 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { DejavuFootball } from "../target/types/dejavu_football";
+import { BN } from "bn.js";
+import { assert } from "chai";
 
 describe("dejavu-football", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.DejavuFootball as Program<DejavuFootball>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  describe('#create-authorizer', async () => {
+    it("creates an authorizer", async () => {
+      const authId = new Date().getTime();
+      const [authorizer] = await anchor.web3.PublicKey.findProgramAddress(
+        [provider.wallet.publicKey.toBuffer(), Buffer.from(`id-${authId}`)],
+        program.programId
+      );
+  
+      await program.methods.createAuthorizer(new BN(authId)).accounts({
+        authorizer: authorizer,
+        user: provider.wallet.publicKey
+      }).rpc();
+  
+      const authorizerData = await program.account.authorizerAccount.fetch(authorizer);
+  
+      assert.ok(
+        authorizerData.authority.equals(provider.wallet.publicKey)
+      );
+    });
   });
+
 });
