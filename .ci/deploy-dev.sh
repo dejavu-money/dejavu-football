c6e29c0c311a28501721f641cb71787b580103e5
Jorge Hernandez <jorge.hernandez@go1.com>
2022-11-05 11:04:30 -0500

    update solana-cli

--- .ci/deploy-dev.sh
+++ .ci/deploy-dev.sh
@@ -7,7 +7,7 @@ export ANCHOR_PROVIDER_URL="https://api.devnet.solana.com"
 export ANCHOR_WALLET='./id.json'
 
 # Airdrop some solanas to the wallet
-solana airdrop 2 $(solana-keygen pubkey $ANCHOR_WALLET) --url https://api.devnet.solana.com && solana airdrop 2 $(solana-keygen pubkey $ANCHOR_WALLET) --url https://api.devnet.solana.com
+(solana airdrop 2 $(solana-keygen pubkey $ANCHOR_WALLET) --url https://api.devnet.solana.com && solana airdrop 2 $(solana-keygen pubkey $ANCHOR_WALLET) --url https://api.devnet.solana.com) || true
 
 # Build program
 anchor build --provider.cluster devnet
